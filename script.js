0231867d2e6ea9854b25ba293f71868834675700
Lstrappare <119477560+Lstrappare@users.noreply.github.com>
2023-12-24 02:42:08 +0000

    He agregado el campo de registro

---
+++ script.js
@@ -0,0 +1,17 @@
+//Preguntas frecuentes
+window.abrirP.addEventListener("click", () => {
+  window.preguntas.showModal();
+});
+
+window.cerrarP.addEventListener("click", () => {
+  window.preguntas.close();
+});
+
+//Terminos y Condiciones
+window.abrirT.addEventListener("click", () => {
+  window.ley.showModal();
+});
+
+window.cerrarT.addEventListener("click", () => {
+  window.ley.close();
+});
