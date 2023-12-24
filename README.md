ac6618552c5bf8feb84c94da7e9e13f9f958ea30
Lstrappare <j.m.cisval1@gmail.com>
2023-12-24 16:06:07 -0600

    Agregué la base de datos y validaciones

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 ## Registrar-Cliente. 🙋
 
 >[!NOTE]
-He agregado el esqueleto del campo de registro, y terminos y condiciones.
+He agregado la base de datos, y las validaciones
 
 >[!TIP]
 El tema de los ID la mayoria esta en el form, y estan escritos de esta manera, apellidoP = apellido paterno :D.
 Puedes usar MDN web docs por si no sabes como se usa alguna etiqueta o función de cualquier lenguaje web.
-Igualmente cada que hagas tus commits, recurda dejar un pequeño mensaje aqui, en notas, en importantes, no importa que >borres lo que he escrito 😁.
+Igualmente cada que hagas tus commits, recurda dejar un pequeño mensaje aqui, en notas, en importantes, no importa que borres lo que he escrito 😁.
 
 >[!IMPORTANT]
 >Falta escribir correctamente las preguntas frecuentes.
-**Si ves "/>" trata de borrarlo**, no se porque mi computadora lo corrige asi, **hoy en dia es una mala practica >:D**.
+Y falta escribir el **script** de inicio de sesión.
+
--- script.js
+++ script.js
@@ -25,4 +25,57 @@ window.cerrarI.addEventListener("click", () => {
   window.iS.close();
 });
 
-//Base de Datos
+
+formulario.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  // Obtener valores de correo y teléfono
+  var email = document.getElementById('email').value;
+  var emailV = document.getElementById('emailV').value;
+  var telefono = document.getElementById('telefono').value;
+  var telefonoV = document.getElementById('telefonoV').value;
+
+  // Reiniciar mensajes de error
+  document.getElementById('noMatchC').innerText = '';
+  document.getElementById('noMatchT').innerText = '';
+
+  // Validar correo y teléfono
+  if (email !== emailV) {
+    document.getElementById('noMatchC').innerText = 'Los correos no coinciden, vuelve a verificarlos.';
+  } else if (telefono !== telefonoV) {
+    document.getElementById('noMatchT').innerText = 'El número de teléfono no coincide, vuelve a verificarlo.';
+  }
+
+  // Verificar si no hay errores
+  if (!document.getElementById('noMatchC').innerText && !document.getElementById('noMatchT').innerText) {
+    // Enviar a la base de datos y redireccionar
+    try {
+      const response = await fetch('https://sheet.best/api/sheets/ec0ee7dc-57a0-44d8-b14d-1f006afbbe58', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "Nombre": formulario.nombre.value,
+          "Apellido Paterno": formulario.apellidoP.value,
+          "Apellido Materno": formulario.apellidoM.value,
+          "Correo electronico": formulario.email.value,
+          "Num telefono": formulario.telefono.value
+        })
+      });
+
+      if (response.ok) {
+        alert('Registro exitoso');
+        window.location.href = 'spa.html';
+      } else {
+        alert('Hubo un problema, verifica los campos.');
+      }
+    } catch (error) {
+      console.error('Error al enviar el formulario:', error);
+      alert('Hubo un error al enviar el formulario');
+    }
+  }
+});
+
+
